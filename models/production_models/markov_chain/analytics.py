806f638558dd7db01d4d93c7902ead535d8646f8
Debanjan Sengupta <debanjan.sengupta1990@outlook.com>
2025-04-16 01:34:55 +0530

    Unused import removed

--- models/production_models/markov_chain/analytics.py
+++ models/production_models/markov_chain/analytics.py
@@ -5,8 +5,6 @@ import uuid
 import os
 import sys
 
-from utils.loggers.json_logger import log_json
-
 # Add project root to Python path
 project_root = os.path.abspath(os.path.join(
     os.path.dirname(__file__), "..", "..", ".."))
